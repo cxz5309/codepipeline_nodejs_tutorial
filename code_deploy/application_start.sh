4c1fa0cc2620f026811a47156c864cc245c9f4de
cxz5309 <cxz5309@gmail.com>
2021-11-12 11:04:05 +0900

    no message

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -27,6 +27,7 @@ echo "pm2 installed"
 # node 어플리케이션 background에서 실행시키기 (by doing so, the server won't be terminated due to inactivates)
 # node app.js 만 입력시 foreground로 실행이 됌
 # node dist/app.js > app.out.log 2> app.err.log < /dev/null & 
+pm2 stop all  
 pm2 kill
 pm2 start node ./bin/www
 pm2 save
