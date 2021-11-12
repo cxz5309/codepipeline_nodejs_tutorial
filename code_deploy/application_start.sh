9ef225dfe5c3da457082fab83962b819920f7937
cxz5309 <cxz5309@gmail.com>
2021-11-12 10:50:25 +0900

    no message

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -28,4 +28,5 @@ echo "pm2 installed"
 # node app.js 만 입력시 foreground로 실행이 됌
 # node dist/app.js > app.out.log 2> app.err.log < /dev/null & 
 pm2 kill
-pm2 start npm -- start
+pm2 start node ./bin/www
+pm2 save
